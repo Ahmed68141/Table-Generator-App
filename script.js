45f3fa715f7c41259576ff0dd7da4aeed8647725
Ahmed68141 <ahmedshaikh68141@gmail.com>
2026-06-19 13:12:49 +0100

    upgraded dependencies

--- script.js
+++ script.js
@@ -4,31 +4,43 @@ let clearButton = document.getElementById("clear-button");
 let messageOutput = document.querySelector(".message-output");
 let generateButton = document.getElementById("generate-button");
 let appContainer = document.querySelector("section.app-card");
+let resultBox = document.querySelector(".result-box");
 let resultOutput = document.createElement("p");
 
 generateButton.addEventListener("click", function() {
     let number = Number(numberInput.value);
     let limit = Number(limitInput.value);
 
-    appContainer.appendChild(resultOutput);
+    resultBox.appendChild(resultOutput);
     resultOutput.innerHTML = "";
     messageOutput.innerHTML = "";
 
     if (limit <= 1500) {
         if (number > 0 && limit > 0) {
+            generateButton.textContent="Generating...";
+            generateButton.disabled=true;
+            setTimeout(function(){
+
             for (let i = 1; i <= limit; i++) {
                 resultOutput.innerHTML += `${number} × ${i} = ${number * i} <br>`;
             }
+            numberInput.value = "";
+            limitInput.value = "";
+            generateButton.textContent="Generate Table"
+            generateButton.disabled=false;
+
+            },0)
+        } else if (numberInput.value === "" || limitInput.value === "") {
+            messageOutput.innerHTML = `<h1 class="calculation-message">Fill both inputs</h1>`;
         } else {
-            messageOutput.innerHTML = `<h1 class="calculation-message">Cannot be calculated</h1>`;
-            appContainer.appendChild(messageOutput);
+            messageOutput.innerHTML = `<h1 class="calculation-message">Zero Cannot be calculated</h1>`;
         }
     } else {
         messageOutput.innerHTML = `<div class="error-message"><h1>Number cannot be greater than 1500 in Limit</h1></div>`;
-        appContainer.appendChild(messageOutput);
     }
 });
 
 clearButton.addEventListener("click", function() {
-    location.reload();
+    resultOutput.innerHTML = "";
+    messageOutput.innerHTML = "";
 });
